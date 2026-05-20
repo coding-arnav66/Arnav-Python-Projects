290bb882e2965e4bcb8b1b6968909d3b15220e3c
Arnav Saket <arnavsaket1411@gmail.com>
2026-05-20 16:54:05 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+data = {}
+print("The computer will take sub and marks for that sub as a input from you...Type \'stop\' when no more subjects are need to be entered. ")
+
+while(True):
+    user = input("Sub: ")
+    if(user == "stop"):
+        break
+    value = int(input("marks: "))
+    data.update({user : value})
+fm = int(input(("You pressed stop. Whats the FM?: ")))
+# print(" ".join(data.keys()))
+def add(a, b):
+    return a + b
+print("The average is:", int((reduce(add, data.values())))/int(len(data.values())))
+print("Percentage is:", (int(reduce(add, data.values()))/fm)*100)
+highest_sub = max(data, key=data.get)
+print("You got highest marks in the subject(s) in which you got:", data[highest_sub])
+lowest_sub = min(data, key=data.get)
+print("Improvement is required in the subject(s) you got:", data[lowest_sub])
+print("Credits:\nWas created by: \"Arnav Saket\"")
+d = int((reduce(add, data.values())))/int(len(data.values()))
+e = (int(reduce(add, data.values()))/fm)*100
+o = open("stats.csv", "w")
+o.write(f"Average-{d}               ")
+o.write(f"Percentage-{e}")
+
+o.close
