7a0b2b63c9ab16d3bd4d51c22762c5c53d6b43a0
Arnav Saket <arnavsaket1411@gmail.com>
2026-05-20 17:00:41 +0530

    Update clear_your_confusion.py

--- clear_your_confusion.py
+++ clear_your_confusion.py
@@ -6,10 +6,10 @@ print ("welcome to clear your confusion \ntype your options one by one and type 
 opt = []
 while True:
     user = input("enter your choice: ")
-    opt.append(user)
+    opt.append(user)                                               #adding the inputs given by user in a list until user types done
 
     if (user.lower() == "done"):
         break
 
     a = random.choice(opt)
-print ("the selected one is:", a)
+print ("the selected one is:", a)                                 #choosing a random stuff and printing it
