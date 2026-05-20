9a63bca672ccb7984c64097a0a8be2a3678355d4
Arnav Saket <arnavsaket1411@gmail.com>
2026-05-20 17:16:23 +0530

    Update water_remainder.py

--- water_remainder.py
+++ water_remainder.py
@@ -1,18 +1,16 @@
 import time
 from plyer import notification
 
-q = int(input("Enter the length of time intervals you want between the notifications: "))
+q = int(input("Enter the length of time intervals you want between the notifications(in seconds): "))    #taking time interval as a input
 print(f"Water remainder will pop out in every {q} seconds...")
 
-def h2o_remainder():
-    while True:
-        notification.notify(
-            title = "Hey! Drink Water 💧",
-            message = "You've been studying hard. Take a sip of water to stay hydrated!",
-            app_icon = None, # You can add a .ico file path here later
-            timeout = 10 # The notification stays for 10 seconds
-        )
-        time.sleep(q)
 
-if __name__ == "__main__":
-    h2o_remainder()        
+while True:
+    notification.notify(
+        title = "Hey! Drink Water 💧",
+        message = "You've been working hard. Take a sip of water to stay hydrated :)",              #the message
+        app_icon = None, 
+        timeout = 20
+    )
+    time.sleep(q)
+    
