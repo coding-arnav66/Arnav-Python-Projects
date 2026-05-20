7be97eb336d5e25daf86169831cf22e66023d4a9
Arnav Saket <arnavsaket1411@gmail.com>
2026-05-20 16:58:45 +0530

    Update data.py

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from functools import reduce
 data = {}
 print("The computer will take sub and marks for that sub as a input from you...Type \'stop\' when no more subjects are need to be entered. ")
 
-while(True):
+while(True):                                                                                       #adding contents in dict-data
     user = input("Sub: ")
     if(user == "stop"):
         break
@@ -13,16 +13,17 @@ fm = int(input(("You pressed stop. Whats the FM?: ")))
 # print(" ".join(data.keys()))
 def add(a, b):
     return a + b
-print("The average is:", int((reduce(add, data.values())))/int(len(data.values())))
-print("Percentage is:", (int(reduce(add, data.values()))/fm)*100)
+print("The average is:", int((reduce(add, data.values())))/int(len(data.values())))                #average
+print("Percentage is:", (int(reduce(add, data.values()))/fm)*100)                                  #percentage
 highest_sub = max(data, key=data.get)
 print("You got highest marks in the subject(s) in which you got:", data[highest_sub])
 lowest_sub = min(data, key=data.get)
-print("Improvement is required in the subject(s) you got:", data[lowest_sub])
+print("Improvement is required in the subject(s) you got:", data[lowest_sub])                      #highest and lowest marks
 print("Credits:\nWas created by: \"Arnav Saket\"")
 d = int((reduce(add, data.values())))/int(len(data.values()))
 e = (int(reduce(add, data.values()))/fm)*100
-o = open("stats.csv", "w")
+o = open("stats.csv", "w")                                                                         #writing the contents in a csv file
+o.write(a.items())               
 o.write(f"Average-{d}               ")
 o.write(f"Percentage-{e}")
 
